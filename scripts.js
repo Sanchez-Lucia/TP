1cbb65cee670f8dec06a370f597b7bbb669fe0ab
Lucía Sánchez <116378686+Sanchez-Lucia@users.noreply.github.com>
2023-11-01 14:43:11 -0300

    trabajo js

---
+++ scripts.js
@@ -0,0 +1,19 @@
+const ticket = 200;
+
+// descuentos
+
+let dscEstudiante = 0.80;
+let dscTrainee = 0.50;
+let dscJunior = 0.15;
+
+
+// Obtenemos elementos donde se van a encontrar los valores a calcular 
+
+const cantidad = document.getElementById('cantidad');
+const categoria = document.getElementById('categoria');
+const totalPagar = document.getElementById('totalAPagar');
+const btnResumen = document.getElementById('resumen');
+
+function calcularPago() {
+    let total = ;
+};
