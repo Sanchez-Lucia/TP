65b05be3a2d91ca4a8fd290533613e1ccf86d9db
Lucia Sanchez-Mac <lucia_sanchez_@hotmail.com>
2023-11-05 21:11:31 -0300

    correccion de validacion de datos

--- scripts.js
+++ scripts.js
@@ -30,18 +30,17 @@ function calcularPago() {
             total = "";
     }
 
-    totalPagar.textContent = `Total a pagar: $ ${total}`;
+    totalPagar.textContent = `Total a Pagar: $ ${total}`;
 
 };
 
 // Prueba para comprobar que haya datos
 
-var objetoCampo = document.getElementsByClassName('form-control');
+const objetoCampo = document.getElementsByClassName('form-control');
 
 function recorrerObjeto() {
     for (let i = 0; i < objetoCampo.length; i++) {
         valorCampo = objetoCampo[i].value
-        console.log(valorCampo);
          if (valorCampo == "") {
             objetoCampo[i].classList.add('is-invalid');
          } else {
@@ -50,12 +49,71 @@ function recorrerObjeto() {
     };
 };
 
+function comprobarCategoria() {
+    if (categoria.textContent == "Sin categoria") {
+        categoria.classList.add('is-invalid');
+    } else {
+        categoria.classList.add('is-invalid');
+    }
+};
+ 
+
+
 btnResumen.addEventListener('click', (evento)=> {
     evento.preventDefault();
     calcularPago();
     recorrerObjeto();
+    comprobarCategoria();
    
 }
 );
+
+categoria.addEventListener('click', ()=> {
+    if (categoria.classList.contains('is-valid')) {
+        categoria.classList.remove('is-valid')
+     } else if (categoria.classList.contains('is-invalid')) {
+         categoria.classList.remove('is-invalid')
+     }
+}
+);
+
+
+
+const objetoInput = document.getElementsByClassName('form-control');
+
+
+objetoInput[0].addEventListener('click', ()=> {
+    if (objetoInput[0].classList.contains('is-valid')) {
+       objetoInput[0].classList.remove('is-valid')
+    } else if (objetoInput[0].classList.contains('is-invalid')) {
+        objetoInput[0].classList.remove('is-invalid')
+    }
+});
+
+objetoInput[1].addEventListener('click', ()=> {
+    if (objetoInput[1].classList.contains('is-valid')) {
+       objetoInput[1].classList.remove('is-valid')
+    } else if (objetoInput[1].classList.contains('is-invalid')) {
+        objetoInput[1].classList.remove('is-invalid')
+    }
+});
+
+objetoInput[2].addEventListener('click', ()=> {
+    if (objetoInput[2].classList.contains('is-valid')) {
+       objetoInput[2].classList.remove('is-valid')
+    } else if (objetoInput[2].classList.contains('is-invalid')) {
+        objetoInput[2].classList.remove('is-invalid')
+    }
+});
+
+objetoInput[3].addEventListener('click', ()=> {
+    if (objetoInput[3].classList.contains('is-valid')) {
+       objetoInput[3].classList.remove('is-valid')
+    } else if (objetoInput[3].classList.contains('is-invalid')) {
+        objetoInput[3].classList.remove('is-invalid')
+    }
+});
+
+
 
 
